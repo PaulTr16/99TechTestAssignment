4821fb0d10908bea0cce8accb11b3c8773957daa
Phuc Tran <phuclong1618@gmail.com>
2025-07-15 09:57:40 +0700

    yaml file

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,9 +30,9 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 4 : undefined,
+  workers: process.env.CI ? 5 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html', { open: 'always' }]],
+  reporter: [['html', 'line']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
