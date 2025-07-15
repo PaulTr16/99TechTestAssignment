812d8d3fcacc682b34495102be578ecfc033de9c
Phuc Tran <phuclong1618@gmail.com>
2025-07-15 10:10:03 +0700

    fix login

--- src/utils/setup.ts
+++ src/utils/setup.ts
@@ -5,5 +5,6 @@ setup("set up for search tests", async ({ loginPage, page }) => {
     loginPage.gotoLoginPage();
     await loginPage.login(process.env.USER_NAME!, process.env.USER_PASSWORD!);
     await page.waitForLoadState("load");
+    await loginPage.expectSuccessfulLogin()
     await page.context().storageState({ path: "storageState.json" });
 })
